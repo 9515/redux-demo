196a8193f94ef510ad507c734b5854a7a433534f
9515 <953857170@qq.com>
2017-11-08 16:54:50 +0800

    完成了同步

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import { connect } from 'react-redux';
+import MyComponent from './mycomponent';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+function mapStateToProps(state) {
+  return {
+    text: state.text,
+    name: state.name
   }
 }
+function mapDispatchToProps(dispatch) {
+  return {
+    onChange: (e) => dispatch({
+      type: 'change',
+      payload: e.target.value
+    })
+  }
+}
+
+const App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MyComponent)
 
 export default App;
---
+++ src/mycomponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+  
+
+class MyComponent extends React.Component {
+    render () {
+        return (
+            <div className="index">
+                <p>{this.props.text}</p>
+                <input
+                defaulivalue={this.props.name}
+                onChange={this.props.onChange} />
+            </div>
+        )
+    }
+}
+export default MyComponent
